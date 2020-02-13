6df6e3fea0c60cc62e45c61764fbf762ef6e4964
Alexander Pronin <pronin.alx@gmail.com>
2020-02-13 19:43:18 +0100

    Initial Commit

---
+++ firebase-auth-demo-ios/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  firebase-auth-demo-ios
+//
+//  Created by Aleksandr Pronin on 13.02.20.
+//  Copyright © 2020 pronin.alx. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
